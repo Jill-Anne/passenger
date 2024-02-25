3a47ae131135f93d6f63b4c72d592a489a44c207
Jill <jillannebragado13@gmail.com>
2024-02-25 23:37:49 +0800

    Profile drawer?menu

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,9 +16,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final Completer<GoogleMapController> googleMapCompleterController =
-      Completer<GoogleMapController>();
+  final Completer<GoogleMapController> googleMapCompleterController = Completer<GoogleMapController>();
   GoogleMapController? controllerGoogleMap;
+  GlobalKey<ScaffoldState> sKey = GlobalKey<ScaffoldState>();
 
   void updateMapTheme(GoogleMapController controller) {
     getJsonFileFromThemes("themes/night_style.json")
@@ -60,6 +60,113 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: sKey,
+      drawer: Container(
+        width: 255,
+        color: Colors.black87,
+
+        child: Drawer(
+          backgroundColor: Colors.white10,
+          
+          child: ListView(
+            children: [
+
+              const Divider(
+                height: 1,
+                color: Colors.grey,
+                thickness: 1,
+              ),
+
+    //header
+              Container(
+                color: Colors.black54,
+                height: 160,
+                child: DrawerHeader(
+                  decoration: const BoxDecoration(
+                    color: Colors.white10,
+                  ),
+                  child: Row(
+                    children: [
+
+                      Image.asset(
+                        "assets/images/avatarman.png",
+                        width: 60,
+                        height: 60,
+                      ),
+
+                      const SizedBox(width: 16,),
+
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+
+                          Text(
+                            userName,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                          const SizedBox(height: 4,),
+
+                          const Text(
+                            "Profile",
+                            style: TextStyle(
+                              color: Colors.white38,
+                            ),
+                          ),
+
+                        ],
+                      ),
+
+                    ],
+                  ),
+                ),
+              ),
+
+              const Divider(
+                height: 1,
+                color: Colors.grey,
+                thickness: 1,
+              ),
+
+              const SizedBox(height: 10,),
+
+  //body
+              ListTile(
+                leading: IconButton(
+                  onPressed: (){},
+                  icon: const Icon(Icons.info, color: Colors.grey,),
+                ),
+                title: const Text("About", style: TextStyle(color: Colors.grey),),
+              ),
+
+              GestureDetector(
+                onTap: ()
+                {
+                  // FirebaseAuth.instance.signOut();
+
+                  // Navigator.push(context, MaterialPageRoute(builder: (c)=> LoginScreen()));
+                },
+                child: ListTile(
+                  leading: IconButton(
+                    onPressed: (){},
+                    icon: const Icon(Icons.logout, color: Colors.grey,),
+                  ),
+                  title: const Text("Logout", style: TextStyle(color: Colors.grey),),
+                ),
+              ),
+
+
+            ],
+          ),
+        ),
+      ),
+
+
+
       body: Stack(
         children: [
           GoogleMap(
@@ -76,6 +183,46 @@ class _HomePageState extends State<HomePage> {
               getCurrentLiveLocationOfUser();
             },
           ),
+
+
+
+
+   //drawer button
+          Positioned(
+            top: 36,
+            left: 19,
+            child: GestureDetector(
+              onTap: ()
+              {
+                sKey.currentState!.openDrawer();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: const
+                  [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 5,
+                      spreadRadius: 0.5,
+                      offset: Offset(0.7, 0.7),
+                    ),
+                  ],
+                ),
+                child: const CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 20,
+                  child: Icon(
+                    Icons.menu,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+
         ],
       ),
     );
